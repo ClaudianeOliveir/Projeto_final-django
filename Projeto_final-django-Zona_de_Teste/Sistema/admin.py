24172ff6af7bb5b38f2e3d3585eb96bed9d9c22a
HandradGit1 <116686731+HandradGit1@users.noreply.github.com>
2023-12-05 21:49:13 -0300

    Delete Projeto_final-django-Zona_de_Teste directory

--- Projeto_final-django-Zona_de_Teste/Sistema/admin.py
+++
@@ -1,8 +0,0 @@
-# No arquivo admin.py dentro do aplicativo "Sistema"
-from django.contrib import admin
-from django.apps import apps
-from .models import Usuario, Categorias, Tarefas
-
-admin.site.register(Usuario)
-admin.site.register(Categorias)
-admin.site.register(Tarefas)
