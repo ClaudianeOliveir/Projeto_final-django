dadda2bef44920dde53620fafaf1bef276fb1190
ClaudianeOliveir <123163214+ClaudianeOliveir@users.noreply.github.com>
2023-12-08 10:25:36 -0300

    Add files via upload
    edicoes

--- Sistema/urls.py
+++ Sistema/urls.py
@@ -7,5 +7,6 @@ from Sistema import views
 urlpatterns = [
     #mapeamento das paginas 
     path('Lista_Tarefas',views.Lista_Tarefas, name='Lista_Tarefas')
+    path('Listas_Criadas', views.Lista_Tarefas, name= 'Lista_Criadas')
     path('Criar_Tarefas', views.Criar_Tarefas, name='Criar_Tarefas')
 ]
--- Sistema/views.py
+++ Sistema/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader 
 
 
 # Create your views here.
 def  Lista_Tarefas(request):
-    print('Lista de Tarefas')
-    return HttpResponse('Lista_Tarefas')  
+    print('Lista de Tarefas').get_template("sistema/base.html")
+    template = loader
+    return HttpResponse(template.render()) 
+
+def Listas_Criadas(request):
+    Print('Listas Criadas')
+    return HttpResponse(Listas_Criadas)
 
 def Criar_Tarefas(request):
     print('Criar Tarefas')
