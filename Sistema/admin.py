de6288330378723ee20c09e8a1d9c5be2c40c414
HandradGit1 <116686731+HandradGit1@users.noreply.github.com>
2023-12-08 16:53:22 -0300

    Mudanças recentes(v2)
    models.py concluído, admin com leves alterações.

--- Sistema/admin.py
+++ Sistema/admin.py
@@ -1,8 +1,8 @@
 # No arquivo admin.py dentro do aplicativo "Sistema"
 from django.contrib import admin
 from django.apps import apps
-from .models import Usuario, Categorias, Tarefas
+from .models import Usuario, Categoria, Tarefa
 
 admin.site.register(Usuario)
-admin.site.register(Categorias)
-admin.site.register(Tarefas)
+admin.site.register(Categoria)
+admin.site.register(Tarefa)
--- Sistema/models.py
+++ Sistema/models.py
@@ -6,28 +6,42 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=50)
     senha = models.CharField(max_length=30)
     email = models.EmailField(max_length=30)##Optei por tipo emailfield mesmo, então usem no padrão de um email real(exemplo@teste.com.br)
+
+    def __str__(self):
+        return f'Usuario {self.nome}'
     class Meta:
         verbose_name_plural = "Usuarios"
-class Tarefas(models.Model):
+class Tarefa(models.Model):
     titulo = models.CharField(max_length=50)
     descricao = models.CharField(max_length=100)
     data_finalizacao = models.DateField()
     concluida = models.BooleanField()
+    proprietario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='tarefas')
 
     def __str__(self):
         return f'{self.descricao}'
-    
-    def Propreitario(self):
-        ##Alterar aqui!!!!! isso é uma chave estrangeira, resolver a relação depois com a class Usuario.
-        pass
 
     class Meta:
         verbose_name_plural = "Tarefas"
-class Categorias(models.Model):
-    nome_cat = models.CharField(max_length=50)
+class Categoria(models.Model):
+    op1 = 'Manutenção'; op2 = 'Movimentação'
+    op3 = 'Compra/Venda'; op4 = 'Desconhecido/Desaparecido'; op5 = 'Stand-by'
+    OPCOES_LISTA = [
+        (op1, 'Manutenção'),
+        (op2, 'Movimentação'),
+        (op3, 'Compra/Venda'),
+        (op4, 'Desconhecido/Desaparecido'),
+        (op5, 'Stand-by')
+    ]
+    nome_categoria = models.CharField(max_length=50)
+    Tarefas = models.ManyToManyField('Tarefa')
+    Tipo = models.CharField(
+        max_length=50,
+        choices=OPCOES_LISTA,
+        default=op5
+    )
 
-    def Tarefas(self):
-        ##Alterar isso aqui!##
-        pass
+    def __str__(self):
+        return f'{self.nome_categoria} --- {self.Tipo}'
     class Meta:
         verbose_name_plural = "Categorias"
