f4ee4b1b277aa70d5db742247d77b8e4c119e969
CamileVit <v.camile@escolar.ifrn.edu.br>
2023-12-09 22:06:47 -0300

    urls e views

--- Sistema/urls.py
+++ Sistema/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin 
-from django.urls import nath 
-from Sistema import views 
-
-#importando views 
+from django.contrib import admin
+from django.urls import path
+from Sistema import views
 
 urlpatterns = [
-    #mapeamento das paginas 
-    path('Lista_Tarefas',views.Lista_Tarefas, name='Lista_Tarefas')
-    path('Listas_Criadas', views.Lista_Tarefas, name= 'Lista_Criadas')
-    path('Criar_Tarefas', views.Criar_Tarefas, name='Criar_Tarefas')
+    # Mapeamento das páginas
+    path('Lista_Tarefas', views.Lista_Tarefas, name='Lista_Tarefas'),
+    path('Listas_Criadas', views.Listas_Criadas, name='Listas_Criadas'),  # Corrigi o nome da função
+    path('Criar_Tarefas', views.Criar_Tarefas, name='Criar_Tarefas'),
 ]
--- Sistema/views.py
+++ Sistema/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader 
+from django.template import loader
 
 
 # Create your views here.
-def  Lista_Tarefas(request):
-    print('Lista de Tarefas').get_template("sistema/base.html")
-    template = loader
-    return HttpResponse(template.render()) 
+def Lista_Tarefas(request):
+    template = loader.get_template("sistema/base.html")  # Corrigi a chamada da função get_template
+    return HttpResponse(template.render())
 
 def Listas_Criadas(request):
-    Print('Listas Criadas')
-    return HttpResponse(Listas_Criadas)
+    print('Listas Criadas')
+    return HttpResponse('Listas Criadas')
 
 def Criar_Tarefas(request):
     print('Criar Tarefas')
-    return HttpResponse(Criar_Tarefas)
+    return HttpResponse('Criar Tarefas')
